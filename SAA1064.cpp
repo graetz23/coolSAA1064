e8289391e6edcb54fbcacba0c298cde4420fa10f
Christian <graetz23@gmail.com>
2014-01-25 19:54:21 +0100

    version 8
    v8 of the SAA1064 ..
    
    - changed int arrey with known digits to byte to be faster - no casting
    - added methods for controlling the brightness of the segments
    - added method to say single digit for a selected segment

--- SAA1064.cpp
+++ SAA1064.cpp
@@ -10,7 +10,7 @@
  * license: Apache V 2.0, Jan 2004
  * created: 24.01.2014
  * edited:  25.01.2014
- * version: 0.6
+ * version: 0.8
  */
 
 /**************************************80**************************************/
@@ -30,13 +30,9 @@
 
 /**************************************80**************************************/
 
-/**************************************80**************************************/
-
 // Constructor
 SAA1064::SAA1064( void ) {
 
-  _arrDigits = 0;
-  
   _init( ); // init all
 
   Wire.begin( );
@@ -46,11 +42,9 @@ SAA1064::SAA1064( void ) {
 // Constructor
 SAA1064::SAA1064( byte i2cAdress ) {
   
-  _arrDigits = 0;
-  
   _init( ); // init all
 
-  _i2cAddress = i2cAdress;
+  _i2cAddress = i2cAdress; // overwrite i2c address set in _init( )
   
   Wire.begin( );
   
@@ -60,160 +54,156 @@ SAA1064::SAA1064( byte i2cAdress ) {
 SAA1064::~SAA1064( void ) {
 
   if( _arrDigits != 0 )
-    delete [ ] _arrDigits;
+    delete [ ] _arrDigits; // is allocated in member method _init( )
 
 } // SAA1064
 
 /**************************************80**************************************/
 
-void // sets the MAPPING TABLE (number to bytes) FOR the used WIRING
-SAA1064::_init( void ) {
-
-    _i2cAddress = 0x70 >> 1; //shifted, due to pin 1 is grounded
-   
-    _arrDigitsLength = 32; // TODO: set later to length
-   
-    _arrDigits = new int[ _arrDigitsLength ];
-    
-	_arrDigits[  0 ] =  63; // 0 : 1+2+4+8+16+32    =  63
-	_arrDigits[  1 ] =   6; // 1 :   2+4     	    =   6
-	_arrDigits[  2 ] =  91; // 2 : 1+2  +8+16   +64 =  91
-	_arrDigits[  3 ] =  79; // 3 : 1+2+4+8      +64 =  79
-	_arrDigits[  4 ] = 102; // 4 :   2+4     +32+64 = 102
-	_arrDigits[  5 ] = 109; // 5 : 1  +4+8   +32+64 = 109
-	_arrDigits[  6 ] = 125; // 6 : 1  +4+8+16+32+64 = 125
-	_arrDigits[  7 ] =   7; // 7 : 1+2+4            =   7
-	_arrDigits[  8 ] = 127; // 8 : 1+2+4+8+16+32+64 = 127
-	_arrDigits[  9 ] = 111; // 9 : 1+2+4+8+  +32+64 = 111
-	_arrDigits[ 10 ] = 119; // A : 1+2+4  +16+32+64 = 119
-	_arrDigits[ 11 ] = 124; // b :    +4+8+16+32+64 = 124
-	_arrDigits[ 12 ] =  57; // C : 1    +8+16+32    =  57
-	_arrDigits[ 13 ] =  94; // d :   2+4+8+16   +64 =  94
-	_arrDigits[ 14 ] = 121; // E : 1    +8+16+32+64 = 121
-	_arrDigits[ 15 ] = 113; // F : 1      +16+32+64 = 113
-	_arrDigits[ 16 ] = 116; // h :     4  +16+32+64 = 116
-	_arrDigits[ 17 ] = 118; // H :   2+4  +16+32+64 = 118
-	_arrDigits[ 18 ] =  56; // L :      +8+16+32    =  56
-	_arrDigits[ 19 ] =  92; // o :     4+8+16   +64 =  92
-	_arrDigits[ 20 ] = 115; // P : 1+2    +16+32+64 = 115
-	_arrDigits[ 21 ] =  80; // r :         16   +64 =  80
-	_arrDigits[ 22 ] =  28; // u :     4+8+16       =  28
-	_arrDigits[ 23 ] =  62; // U :   2+4+8+16+32    =  62
-
-} // SAA1064::_init
-
-/**************************************80**************************************/
-
-void  // update display directly by bytes
-SAA1064::_say( byte b4, byte b3, byte b2, byte b1 ) {
-
-  Wire.beginTransmission( _i2cAddress );
-   
-  Wire.write( 1 ); // instruction byte - first digit to control is 1 (right hand side)
-  
-  Wire.write( b1 ); // digit 1 (RHS)
-  Wire.write( b2 ); // digit 1 (RHS)
-  Wire.write( b3 ); // digit 1 (RHS)
-  Wire.write( b4 ); // digit 1 (RHS)
-	
-  Wire.endTransmission( );
-  
-} // SAA1064::_say
-
-/**************************************80**************************************/
-
-void  // update display directly by hex number: 0 .. F and -1 is blank
-SAA1064::_say( int h4, int h3, int h2, int h1 ) {
-
-   if( h4 < 0 || h4 > _arrDigitsLength ) // secure a valid input of known digits
-     h4 = 0; // blank this segement
-   else
-     h4 = _arrDigits[ h4 ]; // convert to digit to code in bytes
-
-   if( h3 < 0 || h3 > _arrDigitsLength ) // secure a valid input of known digits
-     h3 = 0; // blank this segement
-   else
-     h3 = _arrDigits[ h3 ]; // convert to digit to code in bytes
-
-   if( h2 < 0 || h2 > _arrDigitsLength ) // secure a valid input of known digits
-     h2 = 0; // blank this segement
-   else
-     h2 = _arrDigits[ h2 ]; // convert to digit to code in bytes
-
-   if( h1 < 0 || h1 > _arrDigitsLength ) // secure a valid input of known digits
-     h1 = 0; // blank this segement
-   else
-     h1 = _arrDigits[ h1 ]; // convert to digit to code in bytes
-
-  _say( (byte)( h4 ), (byte)( h3 ), (byte)( h2 ), (byte)( h1 ) ); // cast them
-  
-} // SAA1064::_say
-
-/**************************************80**************************************/
-
 void // set to your own control byte
-SAA1064::set( byte controlbyte ) {
+SAA1064::set( byte controlByte ) {
 
- _controlByte = controlbyte;
-
- Wire.beginTransmission( _i2cAddress );
- Wire.write( B00000000 ); // this is the instruction byte. Zero means the next byte is the control byte
- Wire.write( _controlByte ); // control byte (dynamic mode on, digits 1+3 on, digits 2+4 on, 12mA segment current
- Wire.endTransmission( );
+ _set( controlByte );
  
-}  // SAA1064::setUp
+}  // SAA1064::set
 
 /**************************************80**************************************/
 
-void // switch all segemtns on for testing them
+void // switch all segmetns on for testing them with 3 mA
 SAA1064::setTest( ) {
 
- _controlByte = B01001111;
- 
- Wire.beginTransmission( _i2cAddress );
- Wire.write( B00000000 ); // this is the instruction byte. Zero means the next byte is the control byte
- Wire.write( _controlByte ); // control byte (dynamic mode on, digits 1+3 on, digits 2+4 on, 12mA segment current
- Wire.endTransmission( );
-
+ _set( B00011111 );
+  
 } // SAA1064::setTest
 
 /**************************************80**************************************/
 
-void // set up to a static mode: 2 digits and bright (12 mA)
+void // set up to a static mode: 2 digits and 21 mA as maximum
 SAA1064::setStatic( ) {
   
- _controlByte = B01000110;
-  
- Wire.beginTransmission( _i2cAddress );
- Wire.write( B00000000 ); // this is the instruction byte. Zero means the next byte is the control byte
- Wire.write( _controlByte ); // control byte (dynamic mode on, digits 1+3 on, digits 2+4 on, 12mA segment current
- Wire.endTransmission( );
+ _set( B01110110 );
 
 } // SAA1064::setStatic
 
 /**************************************80**************************************/
 
-void // set up to dynamic mode: 4 digits and bright (12 mA)
+void // set up to dynamic mode: 4 digits and 21 mA as maximum
 SAA1064::setDynamic( ) {
+  
+ _set( B01110111 );
 
-  _controlByte = B01000111;
+} // SAA1064::setDynamic
 
- Wire.beginTransmission( _i2cAddress );
- Wire.write( B00000000 ); // this is the instruction byte. Zero means the next byte is the control byte
- Wire.write( _controlByte ); // control byte (dynamic mode on, digits 1+3 on, digits 2+4 on, 12mA segment current
- Wire.endTransmission( );
+/**************************************80**************************************/
+  
+void // set to output currents 3 mA
+SAA1064::setDark( void ) {
 
-} // SAA1064::setUpDynamic
+  byte controlByte = _controlByte;
+  controlByte = controlByte % B00001000; // isolation of config bytes
+  controlByte += B00010000; // add position of 3 mA
+  set( controlByte );
 
+} // SAA1064::setDark
+
+/**************************************80**************************************/
+
+void // set to output currents 12 mA
+SAA1064::setNormal( void ) {
+
+  byte controlByte = _controlByte;
+  controlByte = controlByte % B00001000; // isolation of config bytes
+  controlByte += B01000000; // add position of 12 mA
+  set( controlByte );
+
+} // SAA1064::setNormal
+
+/**************************************80**************************************/
+
+void //  set to output currents 21 mA
+SAA1064::setBright( void ) {
+
+  byte controlByte = _controlByte;
+  controlByte = controlByte % B00001000; // isolation of config bytes
+  controlByte += B01110000; // add all pos of 3 + 6 + 12 mA
+  set( controlByte );
+
+} // SAA1064::setBright
+  
 /**************************************80**************************************/
 
 void // switch off all segments
 SAA1064::clear( ) {
 
- _say( -1, -1, -1, -1 ); // use number methode; -1 is code for blank
+ say( -1, -1, -1, -1 ); // use number methode; -1 is code for blank
 
 } // SAA1064::clear
 
+/**************************************80**************************************/
+
+void  // update display directly by hex number: 0 .. F and -1 is blank
+SAA1064::say( int h4, int h3, int h2, int h1 ) {
+
+  byte b4 = 0, b3 = 0, b2 = 0, b1 = 0;
+    
+  if( h4 < 0 || h4 > _arrDigitsLength ) // secure a valid input of known digits
+    b4 = 0; // blank this segment
+  else
+    b4 = _arrDigits[ h4 ]; // convert to digit to code in bytes
+
+  if( h3 < 0 || h3 > _arrDigitsLength ) // secure a valid input of known digits
+    b3 = 0; // blank this segment
+  else
+    b3 = _arrDigits[ h3 ]; // convert to digit to code in bytes
+
+  if( h2 < 0 || h2 > _arrDigitsLength ) // secure a valid input of known digits
+    b2 = 0; // blank this segment
+  else
+    b2 = _arrDigits[ h2 ]; // convert to digit to code in bytes
+
+  if( h1 < 0 || h1 > _arrDigitsLength ) // secure a valid input of known digits
+    b1 = 0; // blank this segment
+  else
+    b1 = _arrDigits[ h1 ]; // convert to digit to code in bytes
+
+  _say( b4, b3, b2, b1 ); // cast them
+  
+} // SAA1064::say
+
+/**************************************80**************************************/
+
+void  // update display directly by hex number: 0 .. F and -1 is blank
+SAA1064::say( byte b4, byte b3, byte b2, byte b1 ) {
+
+  _say( b4, b3, b2, b1 ); // cast them
+  
+} // SAA1064::say
+
+/**************************************80**************************************/
+
+void // say byte on single digit: 0, 1, 2 or 3
+SAA1064::say( int h, int digit ) {
+  
+  byte b = 0;
+
+  if( h < 0 || h > _arrDigitsLength ) // secure a valid input of known digits
+    b = 0; // blank this segment
+  else
+    b = _arrDigits[ h ]; // convert to digit to code in bytes
+    
+  _say( b, digit ); // say digit on chosen position
+  
+} // SAA1064::say
+
+/**************************************80**************************************/
+  
+void // say byte on single digit: 0, 1, 2 or 3
+SAA1064::say( byte b, int digit ) {
+
+  _say( b, digit );
+
+} // SAA1064::say
+  
 /**************************************80**************************************/
 
 void // say a number 0 .. 9999 : 42 => 42
@@ -232,7 +222,7 @@ SAA1064::say( int number ) {
  if( digits[ 1 ] == 0 && number < 10 )
    digits[ 1 ] = -1; // library internal code for blanking segement
  
-  _say( digits[ 3 ], digits[ 2 ], digits[ 1 ], digits[ 0 ] );
+  say( digits[ 3 ], digits[ 2 ], digits[ 1 ], digits[ 0 ] );
   
 } // SAA1064::say
 
@@ -245,7 +235,7 @@ SAA1064::sayByZero( int number ) {
   
   _splitNum2Dig( number, digits ); // splt 2 array of digits
  
-  _say( digits[ 3 ], digits[ 2 ], digits[ 1 ], digits[ 0 ] );
+  say( digits[ 3 ], digits[ 2 ], digits[ 1 ], digits[ 0 ] );
  
 } // SAA1064::say
 
@@ -278,12 +268,12 @@ SAA1064::sayTime( int hour, int minute ) {
 	
 	} else {
 	
-	    digit3 = hour % 10; // mod
+	  digit3 = hour % 10; // mod
 		digit4 = hour / 10; // div
 		
 	} // if
 	
-	this->_say( digit4, digit3, digit2, digit1 );
+	say( digit4, digit3, digit2, digit1 );
 	
 } // SAA1064::sayTime
 
@@ -292,7 +282,7 @@ SAA1064::sayTime( int hour, int minute ) {
 void // displays the date
 SAA1064::sayDate( int day, int month ) {
 
-	this->sayTime( day, month ); // same method, fliped positions
+	sayTime( day, month ); // same method, fliped positions
 
 } // sayDate
 
@@ -301,7 +291,7 @@ SAA1064::sayDate( int day, int month ) {
 void // displays the date
 SAA1064::sayDateUS( int day, int month ) {
 
-	this->sayTime( month, day ); // same method, fliped positions
+	sayTime( month, day ); // same method, fliped positions
 
 } // sayDate
 
@@ -319,7 +309,7 @@ SAA1064::sayYear( int year ) {
 void // displays the word Cool
 SAA1064::sayCooL( void ) {
 
-	_say( 12, 19, 19, 18 ); // Hex: 12 -> C, and internal codes for more digits
+	say( 12, 19, 19, 18 ); // Hex: 12 -> C, and internal codes for more digits
 
 } // SAA1064::sayCooL
 
@@ -328,7 +318,7 @@ SAA1064::sayCooL( void ) {
 void // displays the word OooH
 SAA1064::sayOooh( void ) {
 
-	_say( 0, 19, 19, 16 ); // Hex: 0 -> O, and internal codes for more digits
+	say( 0, 19, 19, 16 ); // Hex: 0 -> O, and internal codes for more digits
 
 } // SAA1064::sayOooh
 
@@ -337,7 +327,7 @@ SAA1064::sayOooh( void ) {
 void // displays the word UuuH
 SAA1064::sayUuuh( void ) {
 
-	_say( 23, 22, 22, 16 ); // internal codes for more digits
+	say( 23, 22, 22, 16 ); // internal codes for more digits
 
 } // SAA1064::sayUuuh
 
@@ -346,7 +336,7 @@ SAA1064::sayUuuh( void ) {
 void // displays the word AAAh
 SAA1064::sayAAAh( void ) {
 
-	_say( 10, 10, 10, 16 ); // internal codes for more digits
+	say( 10, 10, 10, 16 ); // internal codes for more digits
 
 } // SAA1064::sayAAAh
 
@@ -355,7 +345,7 @@ SAA1064::sayAAAh( void ) {
 void // displays the word Cool
 SAA1064::sayHAhA( void ) {
 
-	_say( 17, 10, 16, 10 ); // 17 -> H, and internal codes for more digits
+	say( 17, 10, 16, 10 ); // 17 -> H, and internal codes for more digits
 
 } // SAA1064::sayCool
 
@@ -364,7 +354,7 @@ SAA1064::sayHAhA( void ) {
 void // displays the word GoLd
 SAA1064::sayGoLd( void ) {
 
-	_say( 6, 19, 18, 13 ); // 17 -> H, and internal codes for more digits
+	say( 6, 19, 18, 13 ); // 17 -> H, and internal codes for more digits
 
 } // SAA1064::sayGoLd
 
@@ -374,48 +364,142 @@ SAA1064::sayGoLd( void ) {
 void // displays the word Poor
 SAA1064::sayPoor( void ) {
 
-	_say( 20, 19, 19, 21 ); // 17 -> H, and internal codes for more digits
+	say( 20, 19, 19, 21 ); // 17 -> H, and internal codes for more digits
 
 } // SAA1064::sayPoor
 
 /**************************************80**************************************/
 
+void // displays a smiley 8-]
+SAA1064::saySmiley( void ) {
+
+  say( (byte)(0), (byte)(127), byte(64), byte(15) );
+
+} // SAA1064::saySmiley
+
+/**************************************80**************************************/
+  
 void // bytes through all segements by a loop
 SAA1064::byteAll( void ) {
 
-	for( int i = 0; i < 256; i++ ) {
+	for( int i = 0; i < 253; i++ ) {
 	
-	  _say( (byte)( i ), (byte)( i ), (byte)( i ), (byte)( i ) );
+	  _say( (byte)( i+3 ), (byte)( i+2 ), (byte)( i+1 ), (byte)( i ) );
 	  delay( 100 );
 	  
 	} // i
 
 } // SAA1064::byteAll
 
+
+void // show the member _arrDigits with stored codes.
+SAA1064::sayArrDigits( void ) {
+
+ for( int i = 0; i < _arrDigitsLength; i++ ) {
+  
+  _say( _arrDigits[ i ], _arrDigits[ i ], _arrDigits[ i ], _arrDigits[ i ] );
+   delay( 1000 );
+   
+  } // i
+
+} // SAA1064::sayArrDigits
+
 /**************************************80**************************************/
 /**************************************80**************************************/
+/**************************************80**************************************/
+
+void // sets the MAPPING TABLE (number to bytes) FOR the used WIRING
+SAA1064::_init( void ) {
+
+  _i2cAddress = 0x70 >> 1; // shifted, due to pin 1 is grounded (VEE) -> 0x70
+
+  _arrDigitsLength = 24; // length of available char for single segment
+      
+  _arrDigits = new byte[ _arrDigitsLength ]; // dynamic allocation -> del it!
+    
+	_arrDigits[  0 ] =  63; //  0  : 1+2+4+8+16+32    =  63
+	_arrDigits[  1 ] =   6; //  1  :   2+4     	      =   6
+	_arrDigits[  2 ] =  91; //  2  : 1+2  +8+16   +64 =  91
+	_arrDigits[  3 ] =  79; //  3  : 1+2+4+8      +64 =  79
+	_arrDigits[  4 ] = 102; //  4  :   2+4     +32+64 = 102
+	_arrDigits[  5 ] = 109; //  5  : 1  +4+8   +32+64 = 109
+	_arrDigits[  6 ] = 125; //  6  : 1  +4+8+16+32+64 = 125
+	_arrDigits[  7 ] =   7; //  7  : 1+2+4            =   7
+	_arrDigits[  8 ] = 127; //  8  : 1+2+4+8+16+32+64 = 127
+	_arrDigits[  9 ] = 111; //  9  : 1+2+4+8+  +32+64 = 111
+	_arrDigits[ 10 ] = 119; //  A  : 1+2+4  +16+32+64 = 119
+	_arrDigits[ 11 ] = 124; //  b  :    +4+8+16+32+64 = 124
+	_arrDigits[ 12 ] =  57; //  C  : 1    +8+16+32    =  57
+	_arrDigits[ 13 ] =  94; //  d  :   2+4+8+16   +64 =  94
+	_arrDigits[ 14 ] = 121; //  E  : 1    +8+16+32+64 = 121
+	_arrDigits[ 15 ] = 113; //  F  : 1      +16+32+64 = 113
+	_arrDigits[ 16 ] = 116; //  h  :     4  +16+32+64 = 116
+	_arrDigits[ 17 ] = 118; //  H  :   2+4  +16+32+64 = 118
+	_arrDigits[ 18 ] =  56; //  L  :      +8+16+32    =  56
+	_arrDigits[ 19 ] =  92; //  o  :     4+8+16   +64 =  92
+	_arrDigits[ 20 ] = 115; //  P  : 1+2    +16+32+64 = 115
+	_arrDigits[ 21 ] =  80; //  r  :         16   +64 =  80
+	_arrDigits[ 22 ] =  28; //  u  :     4+8+16       =  28
+	_arrDigits[ 23 ] =  62; //  U  :   2+4+8+16+32    =  62
+
+} // SAA1064::_init
+
+/**************************************80**************************************/
+
+void // set a control byte and keep it in mind
+SAA1064::_set( byte controlByte ) {
+
+  _controlByte = controlByte; // store given control byte to member var
+ Wire.beginTransmission( _i2cAddress );
+ Wire.write( B00000000 ); // instruction byte: 0 -> next is the control byte
+ Wire.write( _controlByte ); // have a look in the data sheet for details
+ Wire.endTransmission( );
+ 
+}  // SAA1064::_set
+
+/**************************************80**************************************/
+
+void  // update display directly by bytes
+SAA1064::_say( byte b4, byte b3, byte b2, byte b1 ) {
+
+  Wire.beginTransmission( _i2cAddress );   
+  Wire.write( B00000001 ); // instruction byte -> RHS digit, 2nd, 3rd, last
+  Wire.write( b1 ); // digit 1 (RHS)
+  Wire.write( b2 ); // digit 2 
+  Wire.write( b3 ); // digit 3 
+  Wire.write( b4 ); // digit 4 (LHS)
+  Wire.endTransmission( );
+  
+} // SAA1064::_say
+
+void // say byte on single digit: 1, 2, 3 or 4
+SAA1064::_say( byte b, int digit ) {
+
+  if( digit < 0 || digit > 3 )
+    digit = 0;
+
+  Wire.beginTransmission( _i2cAddress );   
+  Wire.write( (byte)(digit + 1) ); // instruction byte -> 1st, 2nd, 3rd, last
+  Wire.write( b ); // digit 4 (LHS)
+  Wire.endTransmission( );
+
+
+} // SAA1064::_say
+
 /**************************************80**************************************/
 
 void // and returns an array of length 4; allocated before !!!
 SAA1064::_splitNum2Dig( int number, int* digits ) {
 
- int one = 0;
- int ten = 0;
- int hundred = 0;
- int thousand = 0;
-
- thousand = number / 1000; // 1234 -> 1
- hundred = ( number - ( thousand * 1000 ) ) / 100; // 1234 -> 234 -> 2
- ten = ( number - ( thousand * 1000 ) 
-                - ( hundred * 100 ) ) / 10;  // 1234 -> 34 -> 3
- one = number - ( thousand * 1000 ) 
-              - ( hundred * 100 ) 
-			  - ( ten * 10 ); // 1234 -> 4
+  int thousand = number / 1000; // 1234 -> 1
+  int hundred = ( number - ( thousand * 1000 ) ) / 100; // 1234 -> 234 -> 2
+  int ten = ( number - ( thousand * 1000 + hundred * 100 ) ) / 10;  // 1234 -> 34 -> 3
+  int one = number - ( thousand * 1000 + hundred * 100 + ten * 10 ); // 1234 -> 4
  
- digits[ 3 ] = thousand;
- digits[ 2 ] = hundred;
- digits[ 1 ] = ten;
- digits[ 0 ] = one;
+  digits[ 3 ] = thousand;
+  digits[ 2 ] = hundred;
+  digits[ 1 ] = ten;
+  digits[ 0 ] = one;
  
 } // SAA1064::_splitNum2Dig
 
--- examples/Functionality/Functionality.pde
+++ examples/Functionality/Functionality.pde
@@ -12,7 +12,7 @@
  * license: Apache V 2.0, Jan 2004
  * created: 24.01.2014
  * edited:  25.01.2014
- * version: 0.6
+ * version: 0.8
  */
 
 /**************************************80**************************************/
@@ -24,22 +24,44 @@ SAA1064 saa1064;
 
 void setup( ) {
 
-  saa1064.setTest( ); // switch on all segments by SAA1064 internal method
+  saa1064.setTest( );
   
-  delay( 250 );
+  delay( 1000 );
   
-  saa1064.setDynamic( ); // 4 digits and 12 mA of current (bright)
+  saa1064.setDynamic( );
   
-  saa1064.clear( ); // switch all segements off
+  delay( 1000 );
 
-  delay( 250 );
+  saa1064.clear( );
+  delay( 1000 );
   
 } // setup
 
 void loop( ) {
 
-  saa1064.sayCooL( );
-  delay( 1000 );
+  saa1064.setDark( ); // set output currents to 3 mA
+  
+  for( int i = 0; i < 3; i++ ) {
+    
+    saa1064.sayCooL( ); // say CooL
+    delay( 1000 );
+
+    saa1064.say( -1, 5, 10, 10 ); // say _SAA
+    delay( 1000 );
+
+    saa1064.say( 1064 ); // say 1064
+    delay( 1000 );
+    
+    saa1064.saySmiley( ); // say a smiley
+    delay( 1000 );
+    
+    if( i == 0 )
+      saa1064.setNormal( ); // set output currents to 12 mA
+    
+    if( i == 1 )
+      saa1064.setBright( ); // set output currents to 21 mA
+    
+  } // i
  
   saa1064.sayOooh( );
   delay( 1000 );
@@ -59,19 +81,35 @@ void loop( ) {
   saa1064.sayGoLd( );
   delay( 1000 );  
   
-  saa1064.sayTime( 5, 7 );
+  saa1064.say( 815 );
+  delay( 1000 );
+  saa1064.clear( );
   delay( 1000 );
   
-  saa1064.sayTime( 7, 42 );
+  saa1064.sayByZero( 815 );
+  delay( 1000 );
+  saa1064.clear( );
   delay( 1000 );
   
-  saa1064.sayTime( 12, 42 );
+  saa1064.say( 0,8,1,5 );
+  delay( 1000 );  
+  saa1064.clear( );
   delay( 1000 );
   
-  saa1064.sayDate( 7, 1 );
+  saa1064.sayTime( 8, 5 );
   delay( 1000 );
   
-  saa1064.sayDateUS( 7, 1 );
+  saa1064.sayTime( 8, 15 );
+  delay( 1000 );
+  
+  saa1064.sayTime( 18, 15 );
+  delay( 1000 );
+  
+  saa1064.sayDate( 24, 01 );
+  delay( 1000 );
+  saa1064.clear( );
+  delay( 1000 );
+  saa1064.sayDateUS( 01, 24 );
   delay( 1000 );
   
   saa1064.sayYear( 14 );
@@ -80,25 +118,51 @@ void loop( ) {
   saa1064.sayYear( 2014 );
   delay( 1000 );
   
-  saa1064.say( 9 );
+  saa1064.say( 5 );
   delay( 1000 );
-  saa1064.say( 49 );
+  saa1064.say( 15 );
   delay( 1000 );
-  saa1064.say( 249 );
+  saa1064.say( 815 );
   delay( 1000 );
-  saa1064.say( 5249 );
+  saa1064.say( 2815 );
   delay( 1000 );
   
-  saa1064.sayByZero( 9 );
+  saa1064.sayByZero( 5 );
   delay( 1000 );
-  saa1064.sayByZero( 49 );
+  saa1064.sayByZero( 15 );
   delay( 1000 );
-  saa1064.sayByZero( 249 );
+  saa1064.sayByZero( 815 );
   delay( 1000 );
-  saa1064.sayByZero( 5249 );
+  saa1064.sayByZero( 2815 );
   delay( 1000 );
- 
+    
+  saa1064.clear( );
+  saa1064.say( 1, 0 ); // show 1 on 1st digit
+  delay( 1000 );
+  saa1064.clear( );
+  saa1064.say( 2, 1 ); // show 2 on 2nd digit
+  delay( 1000 );
+  saa1064.clear( );
+  saa1064.say( 3, 2 ); // show 3 on 3rd digit
+  delay( 1000 );
+  saa1064.clear( );
+  saa1064.say( 4, 3 ); // show 4 on 4th digit
+  delay( 1000 );    
+    
+  saa1064.sayArrDigits( );
+  delay( 1000 );
+  
+  saa1064.byteAll( );
+  delay( 1000 );
+  
+  for( int i = 0; i < 10000; i++ ) {
+    
+   saa1064.say( i );
+   delay( 10 );
+  
+  } // i
+  
   saa1064.clear( );
   delay( 1000 );
- 
+  
 } // loop
